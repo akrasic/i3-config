d1fc63e9565d8a79d4ace7f1e036031638a410de
Antun Krasic <antun@martuna.co>
2016-01-19 12:10:40 +0100

    Correct the display

--- bin/sensors.sh
+++ bin/sensors.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-gpu_temp=$(sensors k10temp-pci-00c3 | awk '/temp1/ {print $2}')
-cpu_temp=$(sensors nct6776-isa-0290 | awk '/CPUTIN/ {print $2}')
-echo "${cpu_temp}|${gpu_temp}"
+gpu_temp=$(sensors k10temp-pci-00c3 | awk '/temp1/ {print $2}' | sed 's/+//g')
+cpu_temp=$(sensors nct6776-isa-0290 | awk '/CPUTIN/ {print $2}' | sed 's/+//g')
+echo "${cpu_temp} ${gpu_temp}"
