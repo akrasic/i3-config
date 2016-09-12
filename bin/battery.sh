574f9ae388ed73c12e0fafcf470070ad5900be64
Antun Krasic <antun@smartbox.com>
2016-09-12 14:15:19 +0100

    Battery monitor for laptop

---
+++ bin/battery.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+INSTANCE="${1:-BAT0}"
+ALERT_LOW="${2:-10}"
+
+if test -e "/sys/class/power_supply/$INSTANCE"
+then
+    status=$(cat /sys/class/power_supply/$INSTANCE/status)
+    capacity=$(cat /sys/class/power_supply/$INSTANCE/capacity)
+
+    case "$status" in
+        "Discharging")
+            symbol='↓'
+            ;;
+        "Charging")
+            symbol='↑'
+            ;;
+        "Full")
+            exit
+            ;;
+    esac
+
+    # Full text
+    #echo "Ψ $capacity% $symbol"
+
+    # Short text
+    echo "Ψ $capacity% $symbol"
+
+    # Color
+    if test $capacity -le $ALERT_LOW -a $status = "Discharging"
+    then
+        echo "#FF0000"
+    fi
+fi
