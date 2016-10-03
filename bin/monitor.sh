50cd20e71883627f71f86bf5666766138155c9dc
Antun Krasic <antun.krasic@smartbox.com>
2016-10-03 15:34:34 +0100

    Change monitor outpit

---
+++ bin/monitor.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+#
+# Connected to the i3blocks to enable monitors on click
+
+
+xrandr -q | grep -wv eDP1 | grep connected | \
+while read line; do
+    output=$(echo "$line" | awk -F' ' '{print $1}')
+
+    connected=$(echo "$line" | grep -w "connected")
+    activated=$(echo "$line" | grep "connected [0-9]")
+
+    if [[ ! -z "$connected" ]]; then
+        echo "    ${output} "
+
+        if [[ ! -z "$activated" ]]; then
+            [[ -z "${BLOCK_BUTTON}" ]] || {
+                xrandr --output "${output}" --off
+                pkill -RTMIN+4 i3blocks
+            }
+        else
+            [[ -z "${BLOCK_BUTTON}" ]] || {
+                xrandr --auto
+                xrandr --output "${output}" --left-of eDP1
+                pkill -RTMIN+4 i3blocks
+            }
+
+            exit 33
+        fi
+
+        exit 0
+    else
+        if [[ ! -z "$activated" ]]; then
+            echo "    ${output} "
+
+            [[ -z "${BLOCK_BUTTON}" ]] || {
+                xrandr --output "${output}" --off
+                pkill -RTMIN+4 i3blocks
+            }
+
+            exit 33
+        fi
+    fi
+done
